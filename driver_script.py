d88c7f7a24e3c6e842dea1f116764d79ae3bedd1
saakaarb <saakaar28@gmail.com>
2025-08-14 12:34:02 -0700

    adjusted data preproc to allow training trajectories of unequal timestep length

--- driver_script.py
+++ driver_script.py
@@ -1,6 +1,6 @@
-from src.lib.data_processing.classes import Data_Processing
-from src.lib.autoencoder.classes import Encoder_Decoder
-from src.lib.NODE.classes import Neural_ODE
+# from src.lib.data_processing.classes import Data_Processing
+# from src.lib.autoencoder.classes import Encoder_Decoder
+# from src.lib.NODE.classes import Neural_ODE
 from src.utils.classes import ConfigReader,LoggingManager
 from src.utils.helper_functions import log_to_mlflow,log_to_mlflow_artifacts
 import os
@@ -9,6 +9,8 @@ from pathlib import Path
 import mlflow
 
 if __name__=="__main__":
+
+
 
     print(jax.devices())
     # roll all information into a constants dictionary
@@ -21,6 +23,16 @@ if __name__=="__main__":
 
     logging_manager=LoggingManager()
     logging_manager.log("Config file read")
+
+    if config_handler.get_config_status("neural_ode.training.precision")=='float64':
+        jax.config.update("jax_enable_x64", True)
+        logging_manager.log("Using float64 precision")
+        logging_manager.log("Verify dtype:", jax.numpy.array(0.0).dtype)
+
+    # import classes that use jax after setting precision
+    from src.lib.data_processing.classes import Data_Processing
+    from src.lib.autoencoder.classes import Encoder_Decoder
+    from src.lib.NODE.classes import Neural_ODE
 
     # set up mlflow
 
